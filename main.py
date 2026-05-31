482aedcba8b4e1660758d3c852a799e287ff8bf2
DILSHODBEK <dilshodsattar26@gmail.com>
2026-06-01 01:21:51 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class smsPalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = smsPalculateWorker()
+    print("Worker engine initialized.")
